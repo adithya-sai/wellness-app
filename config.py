63d06027bbe0a9ad89d8fce8100e7f64e0595dae
adithyasai <adithyasai.n@gmail.com>
2018-04-01 19:58:35 -0700

    modified config file

--- config.py
+++ config.py
@@ -2,11 +2,9 @@ from pymongo import MongoClient
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Put your secret key here'
-DB_NAME = 'assitiveDB'
+DB_NAME = 'assistiveDB'
 
 DATABASE = MongoClient()[DB_NAME]
-POSTS_COLLECTION = DATABASE.posts
 USERS_COLLECTION = DATABASE.users
-SETTINGS_COLLECTION = DATABASE.settings
 
 DEBUG = True
