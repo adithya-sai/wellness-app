2d0c2205e33f6437d91a9a3710eba4c03da6d5a9
adithyasai <adithyasai.n@gmail.com>
2018-04-02 22:02:14 -0700

    modified git ignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+CORS(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
